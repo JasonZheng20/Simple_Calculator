8b816ffe1d817f73276bccf812bb19f5badacd9f
Jason Zheng <jzzheng@stanford.edu>
2017-06-21 23:57:58 -0700

    woo

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 // simple JS without e36 classes
 var result = document.querySelector(".result");
 
-var clicked = null;
 var currNum = "0";
-var number = "0";
-var currOp = null;
+var number = "";
+var currOp = "";
+var hasOperation = false;
+
+var currNum2 = "";
 
 const nine = document.querySelector(".nine");
 const eight = document.querySelector(".eight");
@@ -28,6 +30,10 @@ const up = document.querySelector(".up");
 const down = document.querySelector(".down");
 
 this.concatNum = this.concatNum.bind(this);
+this.switchFunc = this.switchFunc.bind(this);
+this.compute = this.compute.bind(this);
+this.clearFunc = this.clearFunc.bind(this);
+this.addFunc = this.addFunc.bind(this);
 
 function concatNum() {
   const containerWidth = document.querySelector(".button").offsetWidth * 2;
@@ -37,12 +43,58 @@ function concatNum() {
     console.log(newSize);
     document.querySelector(".result").style.fontSize = newSize;
   }
-  if (currNum == 0) currNum = number;
+  if (currNum == "0" && number != ".") currNum = number;
+  else if (currNum == "0" && number == ".") currNum = "0.";
   else {
     currNum += number;
   }
   document.querySelector(".result").textContent = currNum;
   console.log(currNum);
+}
+
+function switchFunc() {
+  const currentRes = parseFloat(currNum);
+  currNum = -currentRes;
+  document.querySelector(".result").textContent = currNum;
+}
+
+function clearFunc() {
+  if (currOp != "") document.querySelector(currOp).classList.remove("clicked");
+  currNum = "0";
+  number = "";
+  currOp = "";
+  hasOperation = false;
+  document.querySelector(".result").textContent = currNum;
+  //add functionality for history console
+}
+
+function compute() {
+  if (hasOperation) {
+    if (currOp == ".add") {
+      const ultimate = parseFloat(currNum2) + parseFloat(currNum);
+      document.querySelector(".result").textContent = ultimate;
+    }
+    document.querySelector(currOp).classList.remove("clicked");
+  }
+  else {
+    document.querySelector(".result").textContent = parseFloat(currNum);
+  }
+  currNum = "0";
+  number = "";
+  currNum2 = "";
+  currOp = null;
+  hasOperation = false;
+  //buggy, for some reason a call to this.clearFunc doesn't work
+}
+
+function addFunc() {
+  if (!hasOperation) {
+    currOp = ".add";
+    document.querySelector(".add").classList.add("clicked");
+    hasOperation = true;
+    currNum2 = currNum;
+    currNum = "0";
+  }
 }
 
 nine.addEventListener('click', function() {number = "9";});
@@ -74,3 +126,21 @@ one.addEventListener('click', this.concatNum);
 
 zero.addEventListener('click', function() {number = "0";});
 zero.addEventListener('click', this.concatNum);
+
+dec.addEventListener('click', function() {
+  if (!currNum.includes(".")) {
+    number = ".";
+  }
+  else {
+    number = "";
+  }
+  });
+dec.addEventListener('click', this.concatNum);
+
+sign.addEventListener('click', this.switchFunc);
+
+equal.addEventListener('click', this.compute);
+
+clear.addEventListener('click', this.clearFunc);
+
+add.addEventListener('click', this.addFunc);
